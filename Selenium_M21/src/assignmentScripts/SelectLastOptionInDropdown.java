af89cb4f097bc426ed533f7389cd86aa83421984
Madhav Kumar <106333789+2003MADHAV@users.noreply.github.com>
2026-04-17 20:50:51 +0530

    Update

--- Selenium_M21/src/assignmentScripts/SelectLastOptionInDropdown.java
+++ Selenium_M21/src/assignmentScripts/SelectLastOptionInDropdown.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class SelectLastOptionInDropdown {
 
@@ -15,24 +16,17 @@ public class SelectLastOptionInDropdown {
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.get("http://127.0.0.1:5500/dropdown.html");
-		List<WebElement> teams = driver.findElements(By.xpath("//select[@id='team']/option"));
-		System.out.print("Last option in Team is :");
+
+		WebElement team = driver.findElement(By.xpath("//select[@id='team']"));
+		Select select1=new Select(team);
+		List<WebElement> teams = select1.getOptions();
 		System.out.println(teams.get(teams.size()-1).getText());
-		List<WebElement> players = driver.findElements(By.xpath("//select[@id='player']/option"));
-		System.out.print("Last option in player is :");
-		System.out.println(players.get(players.size()-1).getText());
 		
-		//----------------------------------------------------------------------------//
-	/*	
-		System.out.println("Team Count in Options : "+teams.size());
-		for(WebElement team:teams) {
-			System.out.println(team.getText());
-		}
-		System.out.println("Player Count in Options :"+players.size());
-		for(WebElement player:players) {
-			System.out.println(player.getText());
-		}
-	*/	
+		WebElement player = driver.findElement(By.xpath("//select[@id='player']"));
+		Select select2=new Select(player);
+		List<WebElement> players = select2.getOptions();
+		System.out.println(players.get(players.size()-1).getText());
+
 		driver.quit();
 		System.out.println("Pass");
 	}
